e7e29db56da7da18ccabffa35515c26a73daed5b
Tran Van Tung <123tranvantung456@gmail.com>
2025-03-26 20:14:02 +0700

    modify news model

--- api/models.py
+++ api/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 class News(models.Model):
     title = models.TextField()
     content = models.TextField()
-    # excerpt = models.TextField()
+    excerpt = models.TextField(default=title)
     author_id = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=[('draft', 'Draft'), ('published', 'Published')])
     image = models.CharField(max_length=1000, blank=True, null=True)
